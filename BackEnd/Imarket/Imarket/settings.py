715d83b545ec031d950841a4a72aad290a448ad2
Zhanserik <zhanserik.kalmukhambet@gmail.com>
2023-04-25 00:35:43 +0600

    Sending verification code to email : sample

--- BackEnd/Imarket/Imarket/settings.py
+++ BackEnd/Imarket/Imarket/settings.py
@@ -168,4 +168,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'zhanserik.kalmukhambet@gmail.com'
-EMAIL_HOST_PASSWORD = 'JasiK_2004'
+EMAIL_HOST_PASSWORD = 'ifknmeflackbpfoe'
--- BackEnd/Imarket/users/api/urls.py
+++ BackEnd/Imarket/users/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from ..views import send_to_email
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -9,4 +10,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/login/verify/', send_to_email, name='sending to email'),
 ]
