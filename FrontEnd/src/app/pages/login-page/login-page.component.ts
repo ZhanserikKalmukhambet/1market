ca74348716b24d0fc9aeaf0b3d90f844c84ac990
Manapaly <ernat3505@gmail.com>
2023-04-24 19:57:00 +0600

    upd of front jwt

--- FrontEnd/src/app/pages/login-page/login-page.component.ts
+++ FrontEnd/src/app/pages/login-page/login-page.component.ts
@@ -9,34 +9,34 @@ import { LoginService } from "../../services/login/login.service";
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent  {
+export class LoginPageComponent implements OnInit{
 
 
   username: string = '';
   password: string = '';
 
-  // constructor(private loginService: LoginService) {
-  // }
+  constructor(private loginService: LoginService) {
+  }
 
   ngOnInit() {
-    // const token = localStorage.getItem('token');
-    // if (token) {
-    //   LoginService.logged = true;
-    // }
+    const token = localStorage.getItem('token');
+    if (token) {
+      LoginService.logged = true;
+    }
   }
-  // login() {
-  //   this.loginService.login(this.username, this.password).subscribe((data) => {
-  //     localStorage.setItem('token', data.token);
-  //     LoginService.logged = true;
-  //     this.username = '';
-  //     this.password = '';
-  //   });
-  // }
+  login() {
+    this.loginService.login(this.username, this.password).subscribe((data) => {
+      localStorage.setItem('token', data.token);
+      LoginService.logged = true;
+      this.username = '';
+      this.password = '';
+    });
+  }
   //
-  // logout() {
-  //   localStorage.removeItem('token');
-  //   // Request to the Django
-  //   this.logged = false;
-  // }
+  logout() {
+    localStorage.removeItem('token');
+    // Request to the Django
+    this.logged = false;
+  }
 
 }
