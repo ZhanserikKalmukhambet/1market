d4c196aab934b3670dcffa1728faab9b78af323a
Zhanserik <you@example.c git config --global user.name Your>
2023-04-27 19:21:24 +0600

    Login and register by JWT

--- BackEnd/Imarket/users/urls.py
+++ BackEnd/Imarket/users/urls.py
@@ -1,10 +1,11 @@
-from rest_framework import routers
+from django.urls import path
+from .views import send_to_email
+from .views import RegisterView, LoginView, LogoutView, UserView
 
-from .views import UserViewSet
-
-urlpatterns = []
-
-r = routers.DefaultRouter()
-r.register(r'users', UserViewSet)
-
-urlpatterns += r.urls
+urlpatterns = [
+    path('login/verify/', send_to_email, name='sending to email'),
+    path('user/', UserView.as_view(), name='get correct user'),
+    path('register/', RegisterView.as_view(), name='register to the system'),
+    path('login/', LoginView.as_view(), name='authorization'),
+    path('logout/', LogoutView.as_view(), name='log out!')
+]
--- BackEnd/Imarket/users/views.py
+++ BackEnd/Imarket/users/views.py
@@ -1,26 +1,23 @@
+import datetime
+
+import jwt
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.exceptions import AuthenticationFailed
+
 from .models import User
 from .serializers import UserSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.core.mail import send_mail
-from django.conf import Settings
 from django.views.decorators.csrf import csrf_exempt
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 @csrf_exempt
 def send_to_email(request):
     if request.method == 'POST':
-        
         email = request.POST['email']
         message = request.POST['message']
-    
+
         send_mail(
             subject='Verification',
             message=message,
@@ -28,5 +25,71 @@ def send_to_email(request):
             recipient_list=[email, ],
             fail_silently=False
         )
-    
+
     return render(request, 'index.html')
+
+
+class UserView(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('token')
+
+        if not token:
+            return Response({'detail': 'Unauthentificated!'})
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            return Response({'detail': 'Unauthentificated!'})
+
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data)
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.all().filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('User not found!')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Password is incorrect!')
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+        response = Response(token)
+        response.set_cookie(key='token', value=token, httponly=True)
+
+        return response
+
+
+class LogoutView(APIView):
+    def post(self, request):
+        response = Response()
+
+        response.delete_cookie(key='token')
+
+        response.data = {
+            'data': 'Logged out!'
+        }
+
+        return response
