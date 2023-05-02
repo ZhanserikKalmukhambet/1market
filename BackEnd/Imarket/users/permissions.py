02414eef77c8efbda0975b4bd9f10ee5e944a34e
Merey Polatkhan <m_polatkhan@kbtu.kz>
2023-05-02 14:21:25 +0600

    merey-last

---
+++ BackEnd/Imarket/users/permissions.py
@@ -0,0 +1,26 @@
+from rest_framework import permissions
+
+
+class IsSeller(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if obj.seller == request.user:
+            return True
+        return False
+
+
+
+class IsCustomer(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if obj.customer == request.user:
+            return True
+        return False
