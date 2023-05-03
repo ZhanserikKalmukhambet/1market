b6ebea3ae0802503b666f113538ae2fd44baceba
Zhanserik <you@example.c git config --global user.name Your>
2023-05-03 12:22:05 +0600

    Adding more permissions related Order, Product, Cateogory, WarehouseItem

--- BackEnd/Imarket/shop/views.py
+++ BackEnd/Imarket/shop/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
-from users.permissions import IsOwner
+from users.permissions import IsOwnerOfShop, IsOwnerOfWarehouseItem, IsAdminOrReadOnly
 from .models import Shop, WarehouseItem
 from .serializers import ShopSerializer, WarehouseItemSerializer
 
@@ -9,7 +9,7 @@ from .serializers import ShopSerializer, WarehouseItemSerializer
 class ShopViewSet(viewsets.ModelViewSet):
     queryset = Shop.objects.all()
     serializer_class = ShopSerializer
-    permission_classes = (IsOwner, )
+    permission_classes = (IsAdminOrReadOnly, IsOwnerOfShop)
 
     def put_rating_to_shop(self, request, shop_id, new_rating):
         shop = Shop.objects.get(id=shop_id)
@@ -22,6 +22,7 @@ class ShopViewSet(viewsets.ModelViewSet):
 class WarehouseViewSet(viewsets.ModelViewSet):
     queryset = WarehouseItem.objects.all()
     serializer_class = WarehouseItemSerializer
+    permission_classes = (IsAdminOrReadOnly, IsOwnerOfWarehouseItem)
 
     def get_warehouse_items_min_max_price(self, request, min=0, max=1e9):
         queryset = WarehouseItem.objects.filter(price__range=(min, max))
--- BackEnd/Imarket/users/permissions.py
+++ BackEnd/Imarket/users/permissions.py
@@ -3,20 +3,6 @@ from rest_framework import permissions
 from users.choices import Role
 
 
-class IsSeller(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.user and request.user.is_authenticated and request.user.user_type == Role.SELLER:
-            return True
-        return False
-
-
-class IsCustomer(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.user and request.user.is_authenticated and request.user.user_type == Role.CUSTOMER:
-            return True
-        return False
-
-
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
@@ -25,14 +11,60 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         return request.user.is_authenticated and request.user and request.user.is_staff
 
 
-class IsOwner(permissions.BasePermission):
+#####################
+class IsOwnerOfShop(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_authenticated:
+            if request.user.user_type == Role.CUSTOMER:
+                return view.action in ['list', 'retrieve']
+            else:
+                return True
+
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_authenticated:
+            if request.user.user_type == Role.CUSTOMER:
+                return view.action in ['retrieve']
+            elif request.user.user_type == Role.SELLER:
+                return request.user == obj.seller
+
+        return False
+
+
+#################################
+
+class IsOwnerOfWarehouseItem(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            if request.user.user_type == Role.CUSTOMER:
+                return view.action in ['list', 'retrieve']
+            elif request.user.user_type == Role.SELLER:
+                return True
+
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_authenticated:
+            if request.user.user_type == Role.CUSTOMER:
+                return view.action in ['retrieve']
+            elif request.user.user_type == Role.SELLER:
+                return request.user == obj.shop.seller
+
+        return False
+
+
+####################
+
+class IsCustomer(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            if request.user.user_type == Role.SELLER:
+                return view.action in ['list', 'retrieve']
+
             return True
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        return request.user == obj.seller
-
+        return bool(request.user.is_authenticated and
+                    request.user.user_type == Role.CUSTOMER and
+                    request.user == obj.user)
